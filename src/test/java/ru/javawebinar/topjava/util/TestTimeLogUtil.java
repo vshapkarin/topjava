03f831c2e67545a8ce9354ea7121287e1c62a988
Vladimir Shapkarin <vshapkarin@mail.ru>
2019-07-03 22:52:29 +0300

    HW4 fix table log in TTLogUtil

--- src/test/java/ru/javawebinar/topjava/util/TestTimeLogUtil.java
+++ src/test/java/ru/javawebinar/topjava/util/TestTimeLogUtil.java
@@ -1,45 +1,31 @@
 package ru.javawebinar.topjava.util;
 
-import com.google.common.collect.HashBasedTable;
-import com.google.common.collect.Table;
 import org.junit.runner.Description;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 public class TestTimeLogUtil {
 
     private static Logger log = LoggerFactory.getLogger("TimeLogger");
 
-    public static Table<String, String, String> timeTotals = HashBasedTable.create();
-
-    private static final String COL_0 = "Test name";
-    private static final String COL_1 = "Status";
-    private static final String COL_2 = "Time spent(ms)";
+    private static StringBuilder logTable = new StringBuilder("\n")
+            .append("Test name")
+            .append("           Time spent(ms)")
+            .append("\n");
 
     public static void logInfo(Description description, String status, long nanos) {
         String testName = description.getMethodName();
-        String timeSpent = String.valueOf(TimeUnit.NANOSECONDS.toMillis(nanos));
-        timeTotals.put(testName, COL_1, status);
-        timeTotals.put(testName, COL_2, timeSpent);
-        log.info(String.format("Test %s %s, spent %s milliseconds",
+        long timeSpent = TimeUnit.NANOSECONDS.toMillis(nanos);
+        logTable.append(String.format("%-20s", testName))
+                .append(String.format("%-15d", timeSpent))
+                .append("\n");
+        log.info(String.format("Test %s %s, spent %d milliseconds",
                 testName, status, timeSpent));
     }
 
     public static void logTimeTotals() {
-        StringBuilder builder = new StringBuilder("\n\n")
-                .append(String.format("%-20s", COL_0))
-                .append(String.format("%-15s%-15s", timeTotals.columnKeySet().toArray()))
-                .append("\n--------------------------------------------------\n");
-        for (String row : timeTotals.rowKeySet()) {
-            builder.append(String.format("%-20s", row));
-            for (Map.Entry<String, String> entry : timeTotals.row(row).entrySet()) {
-                builder.append(String.format("%-15s", entry.getValue()));
-            }
-            builder.append("\n");
-        }
-        log.info(builder.toString());
+        log.info(logTable.toString());
     }
 }
